a1d16fb85f924e9c45ef67edaa47e8c40f5afa20
ISSA <issa7issa@hotmail.com>
2026-06-27 22:38:55 +0200

    add db port for filess

--- src/config/db.js
+++ src/config/db.js
@@ -6,10 +6,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'eurodating_db',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  charset: 'utf8mb4',
+  charset: 'utf8mb4'
 });
 
 module.exports = pool;
